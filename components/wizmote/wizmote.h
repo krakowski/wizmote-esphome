f76e916c556d78f0ef779c450238484d8f1a1b65
Filip Krakowski <krakowski@hhu.de>
2023-08-21 21:08:41 +0200

    Use custom ESP-NOW packets

--- components/wizmote/wizmote.h
+++ components/wizmote/wizmote.h
@@ -12,24 +12,15 @@ namespace wizmote {
 typedef struct WizMotePacket {
   uint8_t bssid[6];
 
-  uint8_t program;      // 0x91 for ON button, 0x81 for all others
   uint8_t seq[4];       // Incremetal sequence number 32 bit unsigned integer LSB first
-  uint8_t byte5 = 32;   // Unknown
   uint8_t button;       // Identifies which button is being pressed
-  uint8_t byte8 = 1;    // Unknown, but always 0x01
-  uint8_t byte9 = 100;  // Unnkown, but always 0x64
-
-  uint8_t byte10;  // Unknown, maybe checksum
-  uint8_t byte11;  // Unknown, maybe checksum
-  uint8_t byte12;  // Unknown, maybe checksum
-  uint8_t byte13;  // Unknown, maybe checksum
 
   uint32_t sequence;
 
   static inline WizMotePacket build(esp_now::ESPNowPacket espnow_packet) {
     WizMotePacket packet;
     memcpy(packet.bssid, espnow_packet.get_bssid().data(), 6);
-    memcpy(&(packet.program), espnow_packet.get_data().data(), espnow_packet.get_data().size());
+    memcpy(&(packet.seq), espnow_packet.get_data().data(), espnow_packet.get_data().size());
     packet.sequence = encode_uint32(packet.seq[3], packet.seq[2], packet.seq[1], packet.seq[0]);
     return packet;
   }
